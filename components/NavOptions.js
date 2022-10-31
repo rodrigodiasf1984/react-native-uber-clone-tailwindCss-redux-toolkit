06b9ad5fbf24e638f4ae9321f691475b938ac922
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2022-10-31 23:56:05 +0100

    refactor: update styles

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,11 +1,49 @@
-import { View, Text } from 'react-native'
+import { FlatList, TouchableOpacity, Image, View, Text } from 'react-native'
 import React from 'react'
+import tw from 'twrnc'
+import { Icon } from '@rneui/themed'
 
 const NavOptions = () => {
+  const data = [
+    {
+      id: '123',
+      title: 'Get a ride',
+      image: 'https://links.papareact.com/3pn',
+      screen: 'MapScreen'
+    },
+    {
+      id: '456',
+      title: 'Order food',
+      image: 'https://links.papareact.com/28w',
+      screen: 'EatsScreen'
+    }
+  ]
+
   return (
-    <View>
-      <Text>NavOptions</Text>
-    </View>
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+          <View>
+            <Image
+              source={{
+                uri: item.image
+              }}
+              style={{ width: 120, height: 120, resizeMode: 'contain' }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              name='arrowright'
+              color='white'
+              type='antdesign'
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+    />
   )
 }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,20 @@
-import { SafeAreaView, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView, Text, Image, View } from 'react-native'
+import tw from 'twrnc'
+import NavOptions from '../components/NavOptions'
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image
+          source={{
+            uri: 'https://links.papareact.com/gzs'
+          }}
+          style={{ width: 100, height: 100, resizeMode: 'contain' }}
+        />
+        <NavOptions />
+      </View>
     </SafeAreaView>
   )
 }
