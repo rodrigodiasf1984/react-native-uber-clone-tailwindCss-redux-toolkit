8035cf27cd053f5d70ba1563244bdd419169382d
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2022-11-02 23:06:59 +0100

    chore: add new screen

---
+++ screens/MapScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, View, Text } from 'react-native'
+
+const MapScreen = () => {
+  return (
+    <View>
+      <Text>Here is the map stuff</Text>
+    </View>
+  )
+}
+
+export default MapScreen
+
+const styles = StyleSheet.create({})
