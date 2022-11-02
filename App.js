4deac89ef302d3f5b0e99d206d4e6c0403af45bc
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2022-11-02 23:07:56 +0100

    refactor: added stackNavigator

--- App.js
+++ App.js
@@ -1,16 +1,39 @@
+import 'react-native-gesture-handler'
 import React from 'react'
-import { StyleSheet } from 'react-native'
+import { StyleSheet, StatusBar } from 'react-native'
 import { Provider } from 'react-redux'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
 import { store } from './store'
 import HomeScreen from './screens/HomeScreen'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
+import MapScreen from './screens/MapScreen'
 
 export default function App() {
+  const Stack = createStackNavigator()
+  const options = {
+    headerShown: false
+  }
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <StatusBar barStyle='dark-content' backgroundColor={'white'} />
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name='Home'
+              component={HomeScreen}
+              options={options}
+            />
+            <Stack.Screen
+              name='MapScreen'
+              component={MapScreen}
+              options={options}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   )
 }
