91536df1ee1464c3222b56f5da0f79fdeec4b272
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2022-11-04 17:38:08 +0100

    chore: added a Mapscreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import { StyleSheet, View, Text } from 'react-native'
+import tw from 'twrnc'
+import Map from '../components/Map'
+import { createStackNavigator } from '@react-navigation/stack'
 
 const MapScreen = () => {
+  const Stack = createStackNavigator()
+  const options = {
+    headerShown: false
+  }
   return (
     <View>
-      <Text>Here is the map stuff</Text>
+      <View style={tw`h-1/2`}>
+        <Map />
+      </View>
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name='NavigateCard'
+            component={NavigateCard}
+            options={options}
+          />
+        </Stack.Navigator>
+      </View>
     </View>
   )
 }
