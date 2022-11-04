a2b43961cfe06bfc0de1f5f01f9650abaf1c1d0b
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2022-11-04 17:37:32 +0100

    refactor: added google autocomplet

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,16 +2,51 @@ import React from 'react'
 import { SafeAreaView, Text, Image, View } from 'react-native'
 import tw from 'twrnc'
 import NavOptions from '../components/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { GOOGLE_MAPS_APIKEY } from '@env'
+import UberLogo from '../assets/uber_logo.png'
+import { useDispatch } from 'react-redux'
+import { setDestination, setOrigin } from '../slices/navSlice'
 
 const HomeScreen = () => {
+  const dispatch = useDispatch()
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
         <Image
-          source={{
-            uri: 'https://links.papareact.com/gzs'
-          }}
+          source={UberLogo}
           style={{ width: 100, height: 100, resizeMode: 'contain' }}
+        />
+
+        <GooglePlacesAutocomplete
+          placeholder='Where From?'
+          styles={{
+            container: {
+              flex: 0
+            },
+            textInput: {
+              fontSize: 18
+            }
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description
+              })
+            )
+            dispatch(setDestination(null))
+          }}
+          fetchDetails={true}
+          returnKeyType={'search'}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en'
+          }}
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
         />
         <NavOptions />
       </View>
