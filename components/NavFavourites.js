c1293ec4cc411ee1096902e0b9307103ef44ab0d
Rodrigo Dias de Figueiredo <rodrigodiasf@gmail.com>
2022-11-07 23:50:16 +0100

    chore: add new component NavFavourites

---
+++ components/NavFavourites.js
@@ -0,0 +1,60 @@
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  View
+} from 'react-native'
+import React from 'react'
+import { Icon } from '@rneui/themed'
+import tw from 'twrnc'
+
+const data = [
+  {
+    id: '1',
+    icon: 'home',
+    location: 'Home',
+    destination: 'Code Street, Lodon, UK'
+  },
+  {
+    id: '2',
+    icon: 'briefcase',
+    location: 'Work',
+    destination: 'Lodon Eye, Lodon, UK'
+  }
+]
+
+const renderItem = (item) => (
+  <TouchableOpacity>
+    <Text>{item.name}</Text>
+  </TouchableOpacity>
+)
+
+const NavFavourites = () => {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      ItemSeparatorComponent={() => <View style={tw`bg-gray-200 h-[0.2]`} />}
+      renderItem={({ item }) => (
+        <TouchableOpacity style={tw`flex-row items-center p-5`}>
+          <Icon
+            style={tw`bg-gray-300 mr-4 rounded-full p-3`}
+            name={item.icon}
+            type='ionicon'
+            color='white'
+            size={18}
+          />
+          <View>
+            <Text style={tw`font-semibold text-lg`}>{item.location}</Text>
+            <Text style={tw`text-gray-500`}>{item.destination}</Text>
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  )
+}
+
+export default NavFavourites
+
+const styles = StyleSheet.create({})
